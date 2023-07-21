2aa6b6a0895b8ae7c14e81dd0a62d5ccc80d985c
zauser519 <56807402+zauser519@users.noreply.github.com>
2023-07-21 14:09:20 +0900

    Update settings.cpp

--- gray-scott/simulation/settings.cpp
+++ gray-scott/simulation/settings.cpp
@@ -1,60 +1,24 @@
-#include "../../gray-scott/simulation/settings.h"
+#ifndef __SETTINGS_H__
+#define __SETTINGS_H__
 
-#include <fstream>
+#include <string>
 
-#include "../../gray-scott/simulation/json.hpp"
-
-void to_json(nlohmann::json &j, const Settings &s)
+struct Settings
 {
-    j = nlohmann::json{{"nx", s.nx},
-                       {"ny", s.ny},
-                       {"nz", s.nz},
-                       {"direc", s.direc},
-                       {"steps", s.steps},
-                       {"plotgap", s.plotgap},
-                       {"LID", s.LID},
-                       {"WALL", s.WALL},
-                       {"FLUID", s.FLUID},
-                       {"output", s.output},
-                       {"adios_config", s.adios_config}};
-}
+    int nx;
+    int ny;
+    int nz;
+    int direc;
+    int steps;
+    int plotgap;
+    int LID;
+    int WALL;
+    int FLUID;
+    std::string output;
+    std::string adios_config;
 
-void from_json(const nlohmann::json &j, Settings &s)
-{
-    j.at("nx").get_to(s.nx);
-    j.at("ny").get_to(s.ny);
-    j.at("nz").get_to(s.nz);
-    j.at("direc").get_to(s.direc);
-    j.at("steps").get_to(s.steps);
-    j.at("plotgap").get_to(s.plotgap);
-    j.at("LID").get_to(s.LID);
-    j.at("WALL").get_to(s.WALL);
-    j.at("FLUID").get_to(s.FLUID);
-    j.at("output").get_to(s.output);
-    j.at("adios_config").get_to(s.adios_config);
-}
+    Settings();
+    static Settings from_json(const std::string &fname);
+};
 
-Settings::Settings()
-{
-    nx = 100;
-    ny = 100;
-    nz = 100;
-    direc = 19;
-    steps = 20000;
-    plotgap = 200;
-    LID = 2;
-    WALL = 1;
-    FLUID = 0;
-    output = "foo.bp";
-    adios_config = "adios2.xml";
-}
-
-Settings Settings::from_json(const std::string &fname)
-{
-    std::ifstream ifs(fname);
-    nlohmann::json j;
-
-    ifs >> j;
-
-    return j.get<Settings>();
-}
+#endif
